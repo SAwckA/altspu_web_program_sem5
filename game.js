e889864e901989182395809e921a071720958679
SAwckA <saw.cka@mail.ru>
2023-09-25 15:00:03 +0700

    New draw system

--- game.js
+++ game.js
@@ -1,5 +1,101 @@
+import {Person} from "./module.js";
+import {Layout} from "./layout.js";
 
+
+const LAYOUT_SIZE = 61
+
+const KEY_CODE_LEFT = 65
+const KEY_CODE_UP = 87
+const KEY_CODE_RIGHT = 68
+const KEY_CODE_DOWN = 83
 
 export class Game {
-    // TODO: Перенести всю логику управления и отображения игры сюда
+    constructor() {
+        this.startGame()
+        document.addEventListener('keydown', this.addKeyHandler());
+    }
+
+    startGame() {
+        this.person = new Person([1, 1], LAYOUT_SIZE);
+        this.layout = new Layout(LAYOUT_SIZE, this.complete(), this.person);
+        this.GameRootElement = null;
+        this.HTMLCovertedLayout = null;
+
+        this.initialDraw()
+    }
+
+    initialDraw() {
+        let layout = this.layout.getObjectLayout()
+
+        this.GameRootElement = document.getElementById('root')
+
+        let html = '';
+        for (let i = 0; i < layout.length; i++) {
+            html += `<div class="row">`
+            for (let j = 0; j < layout.length; j++) {
+                html += `<div class="cell" id="${layout[i][j].cssID}"></div>`
+            }
+            html += `</div>`
+        }
+
+        this.GameRootElement.innerHTML = html;
+        this.HTMLCovertedLayout = this.GameRootElement.querySelectorAll('.cell')
+    }
+
+    updateDraw() {
+        let layout = this.layout.getObjectLayout()
+
+        for (let i = 0; i < this.HTMLCovertedLayout.length; i++) {
+            let HTMLObject = this.HTMLCovertedLayout[i]
+            let layoutObject = layout[~~(i / layout.length)][i % layout.length]
+
+            if (HTMLObject.id !== layoutObject) {
+                HTMLObject.id = layoutObject.cssID
+            }
+
+            if (HTMLObject.id === 'person') {
+                HTMLObject.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center',
+                    inline: 'center'
+                })
+            }
+        }
+    }
+
+    addKeyHandler() {
+        let game = this
+        return (e) => {
+            game.handleKey(e)
+        }
+    }
+
+    handleKey(e) {
+        switch (e.keyCode) {
+            case KEY_CODE_LEFT:
+                this.person.moveLeft(this.layout.getObjectLayout())
+                break
+
+            case KEY_CODE_UP:
+                this.person.moveUp(this.layout.getObjectLayout())
+                break
+
+            case KEY_CODE_RIGHT:
+                this.person.moveRight(this.layout.getObjectLayout())
+                break
+
+            case KEY_CODE_DOWN:
+                this.person.moveDown(this.layout.getObjectLayout())
+                break
+
+        }
+        this.updateDraw()
+    }
+
+    complete() {
+        let game = this
+        return () => {
+            game.startGame()
+        }
+    }
 }
